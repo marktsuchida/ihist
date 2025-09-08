edf60d0105f0371cb517f4931f39ca6da9abe32a
Mark A. Tsuchida <matsuchida@wisc.edu>
2025-09-08 12:47:12 -0500

    Fix use of ihist as static library on Windows
    Make sure __declspec(dllimport) is not used in this case.

--- include/ihist/ihist.h
+++ include/ihist/ihist.h
@@ -9,18 +9,22 @@
 #include <stddef.h>
 #include <stdint.h>
 
+// Consumers of ihist should define IHIST_SHARED when linking to a shared
+// library build of ihist. But it is optional (in theory, slightly better
+// performance on Windows; probably negligible in practice).
+
 #if defined _WIN32 || defined __CYGWIN__
-#ifdef IHIST_BUILDING_SHARED
+#if defined(IHIST_BUILDING_SHARED)
 #define IHIST_PUBLIC __declspec(dllexport)
-#else
+#elif defined(IHIST_SHARED)
 #define IHIST_PUBLIC __declspec(dllimport)
 #endif
-#else
-#ifdef IHIST_BUILDING_SHARED
+#elif defined(IHIST_BUILDING_SHARED)
 #define IHIST_PUBLIC __attribute__((visibility("default")))
-#else
-#define IHIST_PUBLIC
 #endif
+
+#ifndef IHIST_PUBLIC
+#define IHIST_PUBLIC
 #endif
 
 #ifdef __cplusplus
